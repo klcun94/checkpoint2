30782b160278601f81f1ec424e0aa0cbb1b3481d
klcun94 <klcun94@icloud.com>
2020-05-09 20:43:56 -0500

    add model

---
+++ Models/Workouts.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace capstone.Models
+{
+    public class Workouts
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        //public string Date { get; set; }
+        public string Workout { get; set; }
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+        public int Weight { get; set; }
+        //public Guid UserId { get; set; }
+        //public ApplicationUser User { get; set; }
+    }
+}
